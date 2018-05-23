892a94453b9f462430be327c3d374ae02a6e91ce
kidzen <kidzenfxc@gamil.com>
2018-05-23 17:03:04 +0800

    update

--- common/components/Notify.php
+++ common/components/Notify.php
@@ -12,46 +12,71 @@ use Yii;
 
 class Notify {
 
+    public $config;
+    public $key;
     public $message;
     public $duration;
+    public $title;
 
-    public function fail($message = null, $duration = 8000) {
-        return Yii::$app->session->setFlash('error', [
-                    'type' => 'danger',
-                    'duration' => $duration,
-                    'icon' => 'glyphicon glyphicon-remove-sign',
-                    'title' => Yii::t('app',' Fail'),
-                    'message' => isset($message) ? $message : Yii::t('app','Error processing request')
+    public function fail($config = null) {
+
+        $this->message = isset($config['message']) ? $config['message'] : Yii::t('app','Error processing request');
+        $this->duration = isset($config['duration']) ? $config['duration'] : 8000;
+        $this->title = isset($config['title']) ? $config['title'] : Yii::t('app',' Fail');
+        $this->key = isset($config['key']) ? $config['key'] : 0;
+        // $this->duration = 10000;
+        // $this->duration = 1000000;
+        return Yii::$app->session->setFlash($this->key, [
+            'type' => 'danger',
+            'duration' => $this->duration,
+            'icon' => 'glyphicon glyphicon-remove-sign',
+            'title' => $this->title,
+            'message' => $this->message,
         ]);
     }
 
-    public function success($message = null, $duration = 3000) {
-        return Yii::$app->session->setFlash('success', [
-                    'type' => 'success',
-                    'duration' => $duration,
-                    'icon' => 'glyphicon glyphicon-check-sign',
-                    'title' => Yii::t('app',' Success'),
-                    'message' => isset($message) ? $message : Yii::t('app','Request successfull')
+    public function success($config = null) {
+        $this->message = isset($config['message']) ? $config['message'] : Yii::t('app','Error processing request');
+        $this->duration = isset($config['duration']) ? $config['duration'] : 8000;
+        $this->title = isset($config['title']) ? $config['title'] : Yii::t('app',' Fail');
+        $this->key = isset($config['key']) ? $config['key'] : 0;
+
+        return Yii::$app->session->setFlash($this->key, [
+            'type' => 'success',
+            'duration' => $this->duration,
+            'icon' => 'glyphicon glyphicon-check-sign',
+            'title' => $this->title,
+            'message' => $this->message,
         ]);
     }
 
-    public function info($message, $duration = 3000) {
-        return Yii::$app->session->setFlash('info', [
-                    'type' => 'info',
-                    'duration' => $duration,
-                    'icon' => 'glyphicon glyphicon-info-sign',
-                    'title' => Yii::t('app',' Info'),
-                    'message' => $message
+    public function info($config = null) {
+        $this->message = isset($config['message']) ? $config['message'] : Yii::t('app','Info Message');
+        $this->duration = isset($config['duration']) ? $config['duration'] : 8000;
+        $this->title = isset($config['title']) ? $config['title'] : Yii::t('app',' Info');
+        $this->key = isset($config['key']) ? $config['key'] : 0;
+
+        return Yii::$app->session->setFlash($this->key, [
+            'type' => 'info',
+            'duration' => $this->duration,
+            'icon' => 'glyphicon glyphicon-info-sign',
+            'title' => $this->title,
+            'message' => $this->message,
         ]);
     }
 
-    public function warning($message, $duration = 3000) {
-        return Yii::$app->session->setFlash('warning', [
-                    'type' => 'warning',
-                    'duration' => $duration,
-                    'icon' => 'glyphicon glyphicon-exclamation-mark',
-                    'title' => Yii::t('app',' Warning'),
-                    'message' => $message
+    public function warning($config = null) {
+        $this->message = isset($config['message']) ? $config['message'] : Yii::t('app','Something is wrong');
+        $this->duration = isset($config['duration']) ? $config['duration'] : 8000;
+        $this->title = isset($config['title']) ? $config['title'] : Yii::t('app',' Warning');
+        $this->key = isset($config['key']) ? $config['key'] : 0;
+
+        return Yii::$app->session->setFlash($this->key, [
+            'type' => 'warning',
+            'duration' => $this->duration,
+            'icon' => 'glyphicon glyphicon-exclamation-mark',
+            'title' => $this->title,
+            'message' => $this->message,
         ]);
     }
 
